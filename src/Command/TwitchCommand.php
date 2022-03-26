3f5219f4e938e7b97dc00c8a7aee47c6ba66a544
Vincent UNG <ung.vincent+dev@gmail.com>
2022-03-26 09:55:32 -0400

    Rework command signature for easier containerization

--- src/Command/TwitchCommand.php
+++ src/Command/TwitchCommand.php
@@ -18,13 +18,23 @@ class TwitchCommand extends Command
     protected static $defaultName = 'twitch';
 
     /**
+     * Bot account name. Used for the PingBot subscriber.
      * @var string
      */
     protected $twitchBotAccount;
+
     /**
+     * OAuth token to autorize with Twitch IRC chat.
      * @var string
      */
     protected $twitchOauth;
+
+    /**
+     * The twitch channel to join.
+     * 
+     * @var string
+     */
+    protected $twitchChannel;
 
     /**
      * @var Watchdog
@@ -39,20 +49,22 @@ class TwitchCommand extends Command
         string $twitchBotAccount,
         string $twitchOauth,
         Watchdog $watchdog,
+        string $twitchChannel = null,
         string $name = null
     ) {
         parent::__construct($name);
         $this->twitchBotAccount = $twitchBotAccount;
         $this->twitchOauth = $twitchOauth;
+        $this->twitchChannel = $twitchChannel;
         $this->watchdog = $watchdog;
     }
 
     public function configure()
     {
         $this
-            ->addArgument('channel', InputArgument::REQUIRED, 'The Twitch channel you wish to join')
-            ->addOption('colors', 'c', InputOption::VALUE_OPTIONAL, 'Colored output or not', true)
-            ->addOption('say-hello', 'w', InputOption::VALUE_OPTIONAL, 'Bot says "hello" when joining the channel', true)
+            ->addArgument('channel', InputArgument::OPTIONAL, 'The Twitch channel you wish to join. If not provided, command will look for the TWITCH_CHANNEL env variable.')
+            ->addOption('colors', 'c', InputOption::VALUE_NONE | InputOption::VALUE_NEGATABLE, 'Colored output or not. (Default: true)')
+            ->addOption('welcome', 'w', InputOption::VALUE_NONE | InputOption::VALUE_NEGATABLE, 'Bot says "hello" when joining the channel to notify successful connection. (Default: true)')
             ->setDescription('Start the Twitch chat bot.')
             ->setHelp('Connects to [TWITCH_CHANNEL] channel, with the bot account token [TWITCH_OAUTH]');
     }
@@ -60,7 +72,7 @@ class TwitchCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $colors = $input->getOption('colors');
-        $channel = $input->getArgument('channel');
+        $channel = $input->getArgument('channel') ?? $this->twitchChannel;
 
         if (!strlen($channel)) {
             throw new RuntimeException('Missing "Channel" (got empty string)');
@@ -69,7 +81,7 @@ class TwitchCommand extends Command
         $logger = new ConsoleLogger($output);
         $client = new TwitchChatClient($channel, $this->twitchBotAccount, $this->twitchOauth);
 
-        $client->connect($input->getOption('say-hello'));
+        $client->connect($input->getOption('welcome'));
         if (!$client->isConnected()) {
             if ($colors) {
                 $output->writeln('<error>It was not possible to connect.</>');
