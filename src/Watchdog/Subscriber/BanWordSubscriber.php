9570db1b7f1ac7b7c18438d26c09fc34b14a0d8c
Vincent UNG <ung.vincent+dev@gmail.com>
2022-03-28 22:01:54 -0400

    Try out small improvement on BanwordSubscriber

--- src/Watchdog/Subscriber/BanWordSubscriber.php
+++ src/Watchdog/Subscriber/BanWordSubscriber.php
@@ -29,18 +29,30 @@ class BanWordSubscriber implements EventSubscriberInterface, RedisAwareInterface
 
     public function onBanWord(SniffMessageEvent $event)
     {
+        // Remove spaces from $message. They have no impact on the phonetic
         $message = preg_replace("/\s+/", "", $event->getMessage()->getMessage());
         $messageMetaphoneKey = metaphone($message);
         // dump($messageMetaphoneKey);
+
+        // Keep track of banwords already detected
+        // This might improve performances if on banword is in multiple configs
+        $fastcache = [];
         foreach ($this->redisClient->hgetall(BanwordCommand::REDIS_KEY) as $config) {
             $banwords = explode(',', $config);
             $find = 0;
             foreach ($banwords as $banword) {
+                // If $banword has already been detected in $message
+                if (isset($fastcache[$banword])) {
+                    $find++;
+                    continue;
+                }
+
                 $banwordMetaphoneKey = metaphone($banword);
                 // dump($banwordMetaphoneKey);
 
                 // Ban word metaphone key is in the user message
                 if (strstr($messageMetaphoneKey, $banwordMetaphoneKey)) {
+                    $fastcache[$banword] = true;
                     $find++;
                 }
             }
@@ -50,7 +62,7 @@ class BanWordSubscriber implements EventSubscriberInterface, RedisAwareInterface
                 if ($event->getMessage()->hasRankModerator()) {
                     $event->setResponse('C\'est pas bien de narguer les gens!');
                 } else {
-                    $event->setResponse('Je reconnais ca! C\'est motif de ban!');
+                    $event->setResponse('Je reconnais ca! C\'est motif de ban! ⛔');
                 }
 
                 $event->stopPropagation();
